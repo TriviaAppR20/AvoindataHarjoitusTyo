29daf640475affa01366c5724cc9ec45127098d2
rowancape <niklasmk.pihlajaniemi@gmail.com>
2024-11-25 20:53:18 +0200

    Final touches!!!!!!!

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState, useEffect, useContext } from "react";
+import { StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from "react";
+import CustomPicker from "./src/components/CustomPicker"
+import CustomStepper from "./src/components/CustomStepper"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
 
@@ -12,6 +15,7 @@ export default function App() {
   const [apiSessionToken, setApiSessionToken] = useState("");
   const [isTokenEmpty, setIsTokenEmpty] = useState(false);
 
+  const isDarkMode = true;
 
   //fetch categories
   //from the api
@@ -121,26 +125,39 @@ export default function App() {
   };
 
 
-// Fetches session token from API. If successful it is returned.
-  const fetchApiSessionToken = async () => {
+  const generateQuiz = async () => {
     try {
-      const response = await fetch(
-        "https://opentdb.com/api_token.php?command=request"
-      );
-      const data = await response.json();
+      const data = await fetchQuestions();
       if (data.response_code === 0) {
-        return data.token;
+        navigation.navigate("Game", { questions: data.results });
+      } else if (data.response_code === 4) {
+        showTokenEmpty();
       } else {
-        console.error(
-          `Error fetching token: ${data.response_message}. Error code: ${data.response_code}`
-        );
-        Alert.alert(
-          `Error fetching token: ${data.response_message}. Error code: ${data.response_code}`
-        );
+        let errorMessage = "";
+
+        switch (data.response_code) {
+          case 1:
+            errorMessage =
+              "No Results: Could not return results. The API doesn't have enough questions for your query (e.g., asking for 50 questions in a category that only has 20).";
+            break;
+          case 2:
+            errorMessage =
+              "Invalid Parameter: Contains an invalid parameter. Arguments passed aren't valid (e.g., Amount = 'Five' instead of a number).";
+            break;
+          case 3:
+            errorMessage = "Token Not Found: Session Token does not exist.";
+            break;
+          case 5:
+            errorMessage =
+              "Rate Limit Exceeded: Too many requests. Each IP can only access the API once every 5 seconds.";
+            break;
+          default:
+            errorMessage = "An unknown error occurred.";
+        }
+        Alert.alert("Error", errorMessage, [{ text: "OK" }]);
       }
     } catch (err) {
-      console.error(`Unexpected error: ${err}`);
-      Alert.alert(`Unexpected error: ${err}`);
+      console.error(`An error occured: ${err}`);
     }
   };
 
@@ -160,19 +177,270 @@ export default function App() {
   };
 
 
-  return (
-    <View style={styles.container}>
-      <Text> . </Text>
-      <StatusBar style="auto" />
+return (
+    <View style={[styles.page, isDarkMode && dark.page]}>
+      <Modal
+        visible={isTokenEmpty}
+        transparent={true}
+        onRequestClose={hideTokenEmpty}
+      >
+        <View style={styles.modalFull}>
+          <View style={[styles.modalContainer, { height: "40%" }]}>
+            <Text style={{ fontSize: 16, marginBottom: 8 }}>
+              The app has ran out of new questions to ask you with these
+              specific options. Would you like to reset your token?
+            </Text>
+            <TouchableOpacity
+              style={styles.resetButton}
+              onPress={resetApiSessionToken}
+            >
+              <Text style={{ color: "#FFF" }}>RESET</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.closeInfoButton}
+              onPress={hideTokenEmpty}
+            >
+              <Text style={{ color: "#FFF" }}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <Text style={[styles.genericLabel, isDarkMode && dark.genericLabel, { marginTop: 64 }]}>
+        Number of questions
+      </Text>
+      <View style={styles.sliderContainer}>
+        <CustomStepper
+          initialValue={20}
+          minValue={5}
+          maxValue={50}
+          onChange={(newVal) => setAmountOfQuestions(newVal)}
+          styles={[stepperStyles, isDarkMode ? darkStepperStyles : {}]}
+          iconColor={isDarkMode ? "#FFF" : "#000"}
+        />
+      </View>
+
+      <Text style={[styles.genericLabel, isDarkMode && dark.genericLabel]}>
+        Category
+      </Text>
+      <CustomPicker
+        items={[
+          { label: "Any category", value: "" },
+          ...categories.map((cat) => ({
+            label: cat.name,
+            value: cat.id,
+          })),
+        ]}
+        selectedValue={category}
+        onSelect={(value) => setCategory(value)}
+        styles={[pickerStyles, isDarkMode ? darkPickerStyles : {}]}
+        iconColor={isDarkMode ? "white" : "black"}
+      />
+
+      <Text style={[styles.genericLabel, isDarkMode && dark.genericLabel]}>
+        Difficulty
+      </Text>
+      <CustomPicker
+        items={[
+          { label: "Any difficulty", value: "" },
+          { label: "Easy", value: "easy" },
+          { label: "Medium", value: "medium" },
+          { label: "Hard", value: "hard" },
+        ]}
+        selectedValue={difficulty}
+        onSelect={(value) => setDifficulty(value)}
+        styles={[pickerStyles, isDarkMode ? darkPickerStyles : {}]}
+        iconColor={isDarkMode ? "white" : "black"}
+      />
+
+      <Text style={[styles.genericLabel, isDarkMode && dark.genericLabel]}>
+        Question type
+      </Text>
+      <CustomPicker
+        items={[
+          { label: "Any type", value: "" },
+          { label: "Multiple choice", value: "multiple" },
+          { label: "True / False", value: "boolean" },
+        ]}
+        selectedValue={type}
+        onSelect={(value) => setType(value)}
+        styles={[pickerStyles, isDarkMode ? darkPickerStyles : {}]}
+        iconColor={isDarkMode ? "white" : "black"}
+      />
+
+      <Text style={{ color: isDarkMode ? "white" : "black" }}>{generateQueryUrl()}</Text>
+      <TouchableOpacity
+        style={[styles.startButton, isDarkMode && dark.startButton]}
+        onPress={() => generateQuiz()}
+      >
+        <Text style={[styles.buttonText, isDarkMode && dark.buttonText]}>
+          Start Quiz
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "start",
+    alignItems: "center",
+    padding: 24,
+    backgroundColor: "#EDEDED",
+  },
+  modalFull: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.7)",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 32,
+  },
+  modalContainer: {
+    width: "100%",
+    height: "70%",
+    padding: 16,
+    backgroundColor: "#FFF",
+    borderRadius: 32,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  resetButton: {
+    backgroundColor: "#666",
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+  },
+  closeInfoButton: {
+    backgroundColor: "#000",
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 16,
+  },
+  genericLabel: {
+    marginBottom: 8,
+    color: "#000",
+  },
+  sliderContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "auto",
+    width: "100%",
+    marginBottom: 24,
+  },
+  slider: {
+    width: "80%",
+    paddingLeft: 8,
+  },
+  sliderLabel: {
+    fontSize: 16,
+  },
+  picker: {
+    marginBottom: 24,
+    height: 50,
+    width: "100%",
+    backgroundColor: "#fff",
+  },
+  selection: {
+    marginTop: 20,
+    fontSize: 18,
+  },
+  startButton: {
+    paddingVertical: 18,
+    paddingHorizontal: 38,
+    borderWidth: 1,
+    borderRadius: 24,
+    borderColor: "#f87609",
+    backgroundColor: "#FFF",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 400,
+    color: "#f87609",
   },
 });
+
+const dark = StyleSheet.create({
+  page: {
+    backgroundColor: "#121212",
+  },
+  genericLabel: {
+    color: "#FFF",
+  },
+  startButton: {
+    borderColor: "#f87609",
+    backgroundColor: "#000",
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: "#f87609",
+  },
+  sliderLabel: {
+    color: "#FFF",
+  },
+});
+
+const pickerStyles = {
+  button: {
+    marginBottom: 32,
+    borderWidth: 1,
+    borderColor: "#f87609",
+  },
+  item: {
+    backgroundColor: "#FFF",
+  },
+  itemText: {
+    color: "#000",
+  },
+};
+const darkPickerStyles = {
+  button: {
+    backgroundColor: "#000",
+    borderColor: "#f87609",
+  },
+  buttonText: {
+    color: "#FFF",
+  },
+  item: {
+    backgroundColor: "#000",
+  },
+  itemText: {
+    color: "#FFF",
+  },
+};
+
+const stepperStyles = {
+  container: {
+    borderWidth: 1,
+    borderColor: "#f87609",
+    borderRadius: 32,
+    backgroundColor: "#FFF",
+  },
+  button: {
+    paddingHorizontal: 16,
+    paddingVertical: 14,
+    borderRadius: 32,
+  },
+  input: {
+    width: 50,
+    color: "#000"
+  }
+};
+const darkStepperStyles = {
+  container: {
+    backgroundColor: "#000"
+  },
+  buttonText: {
+    color: "#FFF"
+  },
+  input: {
+    color: "#FFF",
+  },
+};
